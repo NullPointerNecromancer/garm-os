0be03a903ade38f1c651e96f402b8277bfe46fe2
NullPointerNecromancer <gh.gnat102@slmails.com>
2026-09-25 17:01:17 +0200

    Add files via upload

---
+++ files/system/etc/profile.d/zz-garm-fastfetch.sh
@@ -0,0 +1,4 @@
+# Garm: visa Garm-loggan i fastfetch istället för Bazzites.
+# Filnamnet börjar på zz- så att den laddas efter Bazzites egna inställningar och vinner.
+alias fastfetch='/usr/bin/fastfetch -c /usr/share/garm/fastfetch.jsonc'
+alias neofetch='/usr/bin/fastfetch -c /usr/share/garm/fastfetch.jsonc'
